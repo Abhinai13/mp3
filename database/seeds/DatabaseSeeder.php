97920d99a44727497c3297a291ddf7cd6617d503
Abhinai <Abhinai13rules@gmail.com>
2019-05-07 01:01:09 -0400

    Added Answer Migrate, Model and added tests for profile and answer tests

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UserTableSeeder::class);
          $this->call(QuestionsTableSeeder::class);
+         $this->call(AnswersTableSeeder::class);
 
     }
 }
